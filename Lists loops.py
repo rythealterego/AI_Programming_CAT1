cfdc60c64a696b0ed26eb8be978b33eef03f2c32
Ryan Gakinya <ryan.gakinya2021@gmail.com>
2025-10-29 18:19:12 +0300

    COMMIT

---
+++ Lists loops.py
@@ -0,0 +1,4 @@
+students = ["Monicah", "Irwine", "Benedict", "Gloria", "Faith", "Teddy", "Sandra", "Drina", "Ryan"]
+
+for index, name in enumerate(students, start=1):
+    print(f"{index}. {name.upper()}")
